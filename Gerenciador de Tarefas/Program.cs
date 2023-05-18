b37a041f4c80f53afece42e6723e446a9955da09
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-05-18 16:48:24 -0300

    principais funcionalidades implementada

--- Gerenciador de Tarefas/Program.cs
+++ Gerenciador de Tarefas/Program.cs
@@ -8,92 +8,143 @@ namespace Gerenciador_de_Tarefas
         private const string FILE_NAME = "Test.txt";
         static void Main(string[] args)
         {
+            bool encerrarPrograma = false;
             bool listaNaTela = false;
+            MostrarDadosNaTela();
+            while (!encerrarPrograma)
+            {
+                
+                int opcao = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (opcao == 1)
+                {
+                    Console.Write("Nome da tarefa:");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Descrição da tarefa: ");
+                    string desc = Console.ReadLine();
+                    Console.Write("Prioridade da tarefa (Baixa, Media e Alta): ");
+                    Prioridade prio = Enum.Parse<Prioridade>(Console.ReadLine());
+                    TarefaList listaDeTarefa = new TarefaList(name, false);
+                    listaDeTarefa.AdicionarTarefa(new Tarefa(desc, prio, false));
 
+                    if (!File.Exists(FILE_NAME))
+                    {
+                        using (FileStream fs = new FileStream(FILE_NAME, FileMode.CreateNew))
+                        {
+                            using (StreamWriter tw = new StreamWriter(fs))
+                            {
+                                tw.WriteLine(listaDeTarefa.Name);
+                                foreach (var tarefa in listaDeTarefa.Tarefas)
+                                {
+
+                                    tw.WriteLine(tarefa);
+                                }
+
+                            }
+                        }
+                        Console.WriteLine("Lista Criada pressione ENTER para voltar ao menu");
+                        Console.ReadLine();
+                        MostrarDadosNaTela();
+
+                    }
+                    else
+                    {
+                        using (StreamWriter sw = new StreamWriter(FILE_NAME, true))
+                        {
+                            sw.WriteLine(listaDeTarefa.Name);
+                            foreach (var tarefa in listaDeTarefa.Tarefas)
+                            {
+                                sw.WriteLine(tarefa);
+                            }
+
+                        }
+                        Console.WriteLine("Lista criada pressione ENTER para voltar ao menu");
+                        Console.ReadLine();
+                        MostrarDadosNaTela();
+;                    }
+                }
+                else if (opcao == 2)
+                {
+                    List<TarefaList> listaTarefa = LerDados();
+
+                    foreach (TarefaList tarefa in listaTarefa)
+                    {
+                        Console.WriteLine($"Nome da lista: {tarefa.Name}");
+                        foreach (Tarefa tarefas in tarefa.Tarefas)
+                        {
+                            Console.WriteLine($"Descrição: \n{tarefas.Descricao} \nPrioridade: {tarefas.Prioridade}");
+                        }
+                        Console.WriteLine("--------------------------------------------------------");
+                        Console.WriteLine();
+
+                    }
+                    listaNaTela = true;
+                    Console.ReadLine();
+                    Console.WriteLine("Aperte ENTER para voltar ao menu");
+                    MostrarDadosNaTela();
+                }
+                else if (opcao == 3)
+                {
+                    if (!listaNaTela)
+                    {
+                        Console.WriteLine("Selecione a opcao de mostrar lista na tela antes de excluir uma lista");
+                        return;
+                    }
+                    List<TarefaList> listaTarefa = LerDados();
+                    Console.Write("Insira o nome da lista que voce deseja excluir: ");
+                    string name = Console.ReadLine();
+                    RemoverListaArquivo(name);
+                    Console.WriteLine();
+                    Console.WriteLine("Voce removeu a lista de tarefa, aperte ENTER para voltar ao menu");
+                    MostrarDadosNaTela();
+                }
+
+            }
+        }
+        static void RemoverListaArquivo(string nomeLista)
+        {
+            List<TarefaList> tarefas = LerDados(); // Ler as listas existentes do arquivo
+
+            // Procurar a lista com base no nome
+            TarefaList listaParaRemover = tarefas.Find(t => t.Name == nomeLista);
+
+            if (listaParaRemover != null)
+            {
+                tarefas.Remove(listaParaRemover); // Remover a lista da lista principal
+
+                // Sobrescrever o arquivo com as listas atualizadas
+                using (StreamWriter sw = new StreamWriter(FILE_NAME))
+                {
+                    foreach (TarefaList tarefaList in tarefas)
+                    {
+                        sw.WriteLine(tarefaList.Name);
+
+                        foreach (Tarefa tarefa in tarefaList.Tarefas)
+                        {
+                            sw.WriteLine(tarefa.Descricao);
+                            sw.WriteLine(tarefa.Prioridade.ToString());
+                        }
+                    }
+                }
+
+                Console.WriteLine("Lista removida do arquivo.");
+            }
+            else
+            {
+                Console.WriteLine("Lista não encontrada no arquivo.");
+            }
+        }
+
+
+        static void MostrarDadosNaTela()
+        {
+            Console.Clear();
             Console.WriteLine("BEM VINDO AO GERENCIADOR DE TAREFAS!");
-            Console.WriteLine();
             Console.WriteLine("Selecione umas das opcoes abaixo:");
             Console.WriteLine("1 - Adionar uma lista de tarefa.");
             Console.WriteLine("2 - Listar as lista de tarefas");
             Console.WriteLine("3 - Remover uma lista");
-            int opcao = int.Parse(Console.ReadLine());
-            if (opcao == 1)
-            {
-                Console.Write("Nome da tarefa:");
-                string name = Console.ReadLine();
-                Console.WriteLine("Descrição da tarefa: ");
-                string desc = Console.ReadLine();
-                Console.Write("Prioridade da tarefa (Baixa, Media e Alta): ");
-                Prioridade prio = Enum.Parse<Prioridade>(Console.ReadLine());
-                TarefaList listaDeTarefa = new TarefaList(name, false);
-                listaDeTarefa.AdicionarTarefa(new Tarefa(desc, prio, false));
-
-                if (!File.Exists(FILE_NAME))
-                {
-                    using (FileStream fs = new FileStream(FILE_NAME, FileMode.CreateNew))
-                    {
-                        using (StreamWriter tw = new StreamWriter(fs))
-                        {
-                            tw.WriteLine(listaDeTarefa.Name);
-                            foreach (var tarefa in listaDeTarefa.Tarefas)
-                            {
-
-                                tw.WriteLine(tarefa);
-                            }
-
-                        }
-                    }
-                }
-                else
-                {
-                    using (StreamWriter sw = new StreamWriter(FILE_NAME, true))
-                    {
-                        sw.WriteLine(listaDeTarefa.Name);
-                        foreach (var tarefa in listaDeTarefa.Tarefas)
-                        {
-                            sw.WriteLine(tarefa);
-                        }
-
-                    }
-                }
-            }
-            else if (opcao == 2)
-            {
-                List<TarefaList> listaTarefa = LerDados();
-
-                foreach (TarefaList tarefa in listaTarefa)
-                {
-                    Console.WriteLine($"Nome da lista: {tarefa.Name}");
-                    foreach (Tarefa tarefas in tarefa.Tarefas)
-                    {
-                        Console.WriteLine($"Descrição: \n{tarefas.Descricao} \nPrioridade: {tarefas.Prioridade}");
-                    }
-                    Console.WriteLine("--------------------------------------------------------");
-                    Console.WriteLine();
-
-                }
-                listaNaTela = true;
-            }
-            else if (opcao == 3)
-            {
-                if(!listaNaTela)
-                {
-                    Console.WriteLine("Selecione a opcao de mostrar lista na tela antes de excluir uma lista");
-                    return;
-                }
-                List<TarefaList> listaTarefa = LerDados();
-                Console.Write("Insira o nome da lista que voce deseja excluir: ");
-                string name = Console.ReadLine();
-                listaTarefa.RemoveAll(t => t.Name == name);
-
-            }
-
-
-        }
-
-        public void MostrarDadosNaTela()
-        {
-
+            Console.WriteLine();
         }
 
         static List<TarefaList> LerDados()
