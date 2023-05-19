34b1eb07c64e1090f4b2144ddb9e8139edd1bd08
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-05-19 12:25:19 -0300

    Correção nas verificação se existe lista, e funcao de sair da aplicação

--- Gerenciador de Tarefas/Program.cs
+++ Gerenciador de Tarefas/Program.cs
@@ -5,120 +5,18 @@ namespace Gerenciador_de_Tarefas
 {
     internal class Program
     {
+        
         private const string FILE_NAME = "Save.data";
         static void Main(string[] args)
         {
             DateTime hora = DateTime.Now;
             bool encerrarPrograma = false;
-            bool listaNaTela = false;
-            MostrarDadosNaTela();
+            
+            
             while (!encerrarPrograma)
             {
+                MostrarDadosNaTela();
 
-                int opcao = int.Parse(Console.ReadLine());
-                Console.Clear();
-                if (opcao == 1)
-                {
-                    Console.Write("Nome da tarefa:");
-                    string name = Console.ReadLine();
-                    Console.WriteLine("Descrição da tarefa: ");
-                    string desc = Console.ReadLine();
-                    Console.Write("Prioridade da tarefa (Baixa, Media e Alta): ");
-                    Prioridade prio = Enum.Parse<Prioridade>(Console.ReadLine());
-                    TarefaList listaDeTarefa = new TarefaList(name, false);
-                    listaDeTarefa.AdicionarTarefa(new Tarefa(desc, prio));
-
-                    if (!File.Exists(FILE_NAME))
-                    {
-                        using (FileStream fs = new FileStream(FILE_NAME, FileMode.CreateNew))
-                        {
-                            using (StreamWriter tw = new StreamWriter(fs))
-                            {
-                                tw.WriteLine(listaDeTarefa.Name);
-                                foreach (var tarefa in listaDeTarefa.Tarefas)
-                                {
-
-                                    tw.WriteLine(tarefa);
-
-                                }
-                                tw.WriteLine(listaDeTarefa.Hora);
-
-                            }
-                        }
-                        Console.WriteLine("Lista Criada pressione ENTER para voltar ao menu");
-                        Console.ReadLine();
-                        MostrarDadosNaTela();
-
-                    }
-                    else
-                    {
-                        using (StreamWriter sw = new StreamWriter(FILE_NAME, true))
-                        {
-                            sw.WriteLine(listaDeTarefa.Name);
-                            foreach (var tarefa in listaDeTarefa.Tarefas)
-                            {
-                                sw.WriteLine(tarefa);
-                            }
-                            sw.WriteLine(listaDeTarefa.Hora);
-                        }
-                        Console.WriteLine("Lista criada pressione ENTER para voltar ao menu");
-                        Console.ReadLine();
-                        MostrarDadosNaTela();
-                        ;
-                    }
-                }
-                else if (opcao == 2)
-                {
-                    if (!File.Exists(FILE_NAME))
-                    {
-                        Console.WriteLine("ainda não existe lista salva");
-                        break;
-                    }
-                    List<TarefaList> listaTarefa = LerDados();
-                    if (listaTarefa == null)
-                    {
-                        Console.WriteLine("ainda não existe lista salva");
-                        break;
-                    }
-
-                    foreach (TarefaList tarefa in listaTarefa)
-                    {
-                        Console.WriteLine($"Nome da lista:          {tarefa.Name}");
-                        Console.WriteLine();
-                        foreach (Tarefa tarefas in tarefa.Tarefas)
-                        {
-                            Console.WriteLine($"Descrição: \n{tarefas.Descricao} \n\nPrioridade: {tarefas.Prioridade}");
-                        }
-                        Console.WriteLine();
-                        Console.WriteLine($"{tarefa.Hora}");
-                        Console.WriteLine("--------------------------------------------------------");
-                        Console.WriteLine();
-
-                    }
-                    listaNaTela = true;
-                    Console.ReadLine();
-                    Console.WriteLine("Aperte ENTER para voltar ao menu");
-                    MostrarDadosNaTela();
-                }
-                else if (opcao == 3)
-                {
-                    if (!listaNaTela)
-                    {
-                        Console.WriteLine("Selecione a opcao de mostrar lista na tela antes de excluir uma lista");
-                        return;
-                    }
-                    List<TarefaList> listaTarefa = LerDados();
-                    Console.Write("Insira o nome da lista que voce deseja excluir: ");
-                    string name = Console.ReadLine();
-                    RemoverListaArquivo(name);
-                    Console.WriteLine();
-                    Console.WriteLine("Voce removeu a lista de tarefa, aperte ENTER para voltar ao menu");
-                    MostrarDadosNaTela();
-                }
-                else
-                {
-                    break;
-                }
 
             }
         }
@@ -149,24 +47,164 @@ namespace Gerenciador_de_Tarefas
                 }
 
                 Console.WriteLine("Lista removida do arquivo.");
+                Console.ReadKey();
             }
             else
             {
-                Console.WriteLine("Lista não encontrada no arquivo.");
+                Console.WriteLine("Lista não encontrada.");
+                Console.ReadKey();
             }
         }
 
 
         static void MostrarDadosNaTela()
         {
-            Console.Clear();
-            Console.WriteLine("BEM VINDO AO GERENCIADOR DE TAREFAS!");
-            Console.WriteLine("Selecione umas das opcoes abaixo:");
-            Console.WriteLine("1 - Adionar uma lista de tarefa.");
-            Console.WriteLine("2 - Listar as lista de tarefas");
-            Console.WriteLine("3 - Remover uma lista");
-            Console.WriteLine("Qualquer outro botao para sair do programa");
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("BEM VINDO AO GERENCIADOR DE TAREFAS!");
+                Console.WriteLine();
+                Console.WriteLine("Selecione umas das opcoes abaixo:");
+                Console.WriteLine();
+                Console.WriteLine("1 - Adionar uma lista de tarefa.");
+                Console.WriteLine("2 - Listar as lista de tarefas");
+                Console.WriteLine("3 - Remover uma lista");
+                Console.WriteLine();
+                Console.WriteLine("4 - Para sair do programa");
+                Console.WriteLine();
+                int opcao = int.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (opcao)
+                {
+                    case 1:
+                        GravarLista();
+                        break;
+                    case 2:
+                        MostrarLista();
+                        Console.WriteLine("Aperte ENTER para voltar ao menu");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+
+                        RemoverLista();
+                        break;
+                    default:
+                        System.Environment.Exit(0);
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Opção escolhida não é valida");
+                Console.ReadKey();
+            }
+        }
+
+        static void GravarLista()
+        {
+            Console.Write("Nome da tarefa: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Descrição da tarefa: ");
+            string desc = Console.ReadLine();
+            Console.Write("Prioridade da tarefa (Baixa, Media e Alta): ");
+            Prioridade prio = Enum.Parse<Prioridade>(Console.ReadLine());
+            TarefaList listaDeTarefa = new TarefaList(name, false);
+            listaDeTarefa.AdicionarTarefa(new Tarefa(desc, prio));
+
+
+            if (!File.Exists(FILE_NAME))
+            {
+                using (FileStream fs = new FileStream(FILE_NAME, FileMode.CreateNew))
+                {
+                    using (StreamWriter tw = new StreamWriter(fs))
+                    {
+                        tw.WriteLine(listaDeTarefa.Name);
+                        foreach (var tarefa in listaDeTarefa.Tarefas)
+                        {
+
+                            tw.WriteLine(tarefa);
+
+                        }
+                        tw.WriteLine(listaDeTarefa.Hora);
+
+                    }
+                }
+                Console.WriteLine("Lista Criada pressione ENTER para voltar ao menu");
+                Console.ReadLine();
+                MostrarDadosNaTela();
+
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(FILE_NAME, true))
+                {
+                    sw.WriteLine(listaDeTarefa.Name);
+                    foreach (var tarefa in listaDeTarefa.Tarefas)
+                    {
+                        sw.WriteLine(tarefa);
+                    }
+                    sw.WriteLine(listaDeTarefa.Hora);
+                }
+                Console.WriteLine("Lista criada pressione ENTER para voltar ao menu");
+                Console.ReadLine();
+                MostrarDadosNaTela();
+                
+            }
+        }
+
+        static void RemoverLista()
+        {
+            if (!File.Exists(FILE_NAME))
+            {
+                Console.WriteLine("ainda não existe lista salva");
+                Console.ReadKey();
+                return;
+            }
+            List<TarefaList> listaTarefa = LerDados();
+            if (listaTarefa.Count == 0)
+            {
+                Console.WriteLine("ainda não existe lista salva");
+                Console.ReadKey();
+                return;
+            }
+            MostrarLista();
+            Console.WriteLine();            
+            Console.WriteLine("Informe o nome da Lista que deseja remover:");                   
+            string name = Console.ReadLine();
+            RemoverListaArquivo(name);
             Console.WriteLine();
+            Console.WriteLine("Voce removeu a lista de tarefa, aperte ENTER para voltar ao menu");
+        }
+        static void MostrarLista()
+        {
+            if (!File.Exists(FILE_NAME))
+            {
+                Console.WriteLine("ainda não existe lista salva");
+                Console.ReadKey();
+                return;
+            }
+            List<TarefaList> listaTarefa = LerDados();
+            if (listaTarefa.Count == 0)
+            {
+                Console.WriteLine("ainda não existe lista salva");
+                Console.ReadKey();
+                return;
+            }
+
+            foreach (TarefaList tarefa in listaTarefa)
+            {
+                Console.WriteLine($"Nome da lista:          {tarefa.Name}");
+                Console.WriteLine();
+                foreach (Tarefa tarefas in tarefa.Tarefas)
+                {
+                    Console.WriteLine($"Descrição: \n{tarefas.Descricao} \n\nPrioridade: {tarefas.Prioridade}");
+                }
+                Console.WriteLine();
+                Console.WriteLine($"{tarefa.Hora}");
+                Console.WriteLine("--------------------------------------------------------");
+                Console.WriteLine();
+
+            }
         }
 
         static List<TarefaList> LerDados()
